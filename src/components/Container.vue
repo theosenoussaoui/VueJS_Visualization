7d31975e56b94a8f85ec64863f3fa094bf92afff
Théo Senoussaoui <theosen95@gmail.com>
2019-03-19 20:43:44 +0100

    adding vue file for templatings

---
+++ src/components/Container.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="uk-text-lead">Container</div>
+</template>
+
+<script>
+  export default {
+    name: 'Container',
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  background-color: #000;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="uk-text-lead">NAVBAR</div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  background-color: #000;
+}
+</style>
