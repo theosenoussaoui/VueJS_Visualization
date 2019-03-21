8c4465bc0d64ab052688e80dbc3f2b4f762d62ae
Théo Senoussaoui <theosen95@gmail.com>
2019-03-21 18:44:40 +0100

    fixs front + scss

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -65,7 +65,10 @@
             <h3>Vous souhaitez y aller pour le travail ? Les vacances ou bien pour vos études ? Notre site permet de répondre au mieux à vos questions en fonction de la catégorie sélectionnée.</h3>
             <h3>Alors n’hésitez pas à découvrir notre carte Daily Map !</h3>
           </div>
-          <div class="uk-background-contain" style="background-image: url('img/presentation-2.jpg'); height:90vh"></div>
+          <div
+            class="uk-background-contain"
+            style="background-image: url('img/presentation-2.jpg'); height:91vh"
+          ></div>
         </div>
       </div>
     </div>
@@ -96,12 +99,19 @@
         <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
         <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle uk-margin-top" uk-grid>
           <div class="uk-padding-large">
+            <h2 class="title">Présentation</h2>
             <h3>En 2019, lors du Hackathon, nous avons créé une carte interactive pour le tourisme à destination des particuliers. Nous pensions qu’il fallait répondre à une vraie demande d’informations venant de toutes ces personnes voulant avoir des renseignements sur un pays étrangers.</h3>
           </div>
-          <div class="uk-background-contain uk-margin-top" style="background-image: url('img/presentation-1.jpg'); background-size : 550px; min-height : 75vh"></div>
+          <div
+            class="uk-background-contain uk-margin-top"
+            style="background-image: url('img/presentation-1.jpg'); background-size : 550px; min-height : 75vh"
+          ></div>
         </div>
         <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
-          <div class="uk-background-contain" style="background-image: url('img/presentation-2.jpg'); min-height : 95vh"></div>
+          <div
+            class="uk-background-contain"
+            style="background-image: url('img/presentation-2.jpg'); min-height : 95vh"
+          ></div>
           <div class="uk-padding-large">
             <h3>Ensuite, il a été nécessaire d’apporter une réponse personnalisée à toutes les demandes en fonction de la nature du séjour. Nous avons défini 3 natures de voyage : pour les études à l'étranger, pour les vacances et pour le travail (courte et longue durée). Cela nous a permis de délivrer une réponse adaptée.</h3>
             <h3>Aujourd’hui, notre site compte plusieurs milliers d’articles de voyages.</h3>
@@ -109,14 +119,22 @@
         </div>
         <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle uk-padding-middle" uk-grid>
           <div class="uk-padding-large">
-            <h3>Notre objectif ?</h3>
+            <h2 class="title">Notre objectif ?</h2>
             <h3>A chaque question d’un particulier, il faut déclencher une réponse réactive, adaptée et surtout de proximité.Nous orientons aussi les clients vers les sites partenaires afin de les aider et les conseiller au mieux dans la planification de leurs voyages.</h3>
           </div>
-          <div class="uk-background-contain" style="background-image: url('img/presentation-3.jpg'); background-size : 450px; min-height : 55vh"></div>
+          <div
+            class="uk-background-contain"
+            style="background-image: url('img/presentation-3.jpg'); background-size : 450px; min-height : 55vh"
+          ></div>
         </div>
         <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
-          <div class="uk-background-contain" style="background-image: url('img/presentation-4.jpg'); background-size : 350px; min-height : 75vh"></div>
+          <div
+            class="uk-background-contain"
+            style="background-image: url('img/presentation-4.jpg'); background-size : 350px; min-height : 75vh"
+          ></div>
           <div class="uk-padding-large">
+            <h2 class="title">Nos points forts?</h2>
+
             <h3>- Notre réseau de partenaires : Opodo,TripAdvisor,Welink, Hotels.com et AirBnB</h3>
             <h3>- La qualité de nos partenaires</h3>
           </div>
@@ -182,7 +200,7 @@
                   <h3>Quelles sont les catégories d’articles ?</h3>
                 </a>
                 <div class="uk-accordion-content">
-                  <ul>
+                  <ul class="uk-list">
                     <li>À la une</li>
                     <li>Économie</li>
                     <li>Divertissement</li>
@@ -212,7 +230,11 @@
               </li>
             </ul>
           </div>
-          <div class="uk-background-contain" style="background-image: url('img/faq.jpg');" uk-height-viewport></div>
+          <div
+            class="uk-background-contain"
+            style="background-image: url('img/faq.jpg');"
+            uk-height-viewport
+          ></div>
         </div>
       </div>
     </div>
@@ -220,12 +242,11 @@
 </template>
 
 <script>
-  export default {
-    name: "Navbar"
-  };
-
+export default {
+  name: "Navbar"
+};
 </script>
 
 <style lang="scss" scoped>
-  @import "../assets/scss/navbar.scss";
+@import "../assets/scss/custom.scss";
 </style>
